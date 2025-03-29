f7befdfb283b04cb908fba37d504b3ed86be9ef7
Daniela <daniela.primacov@yahoo.com>
2025-03-29 16:47:19 +0100

    Added Hide Form Button

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import { Button, Divider } from "antd";
+
 import FoodBox from "../components/FoodBox";
 import AddFoodForm from "../components/AddFoodForm";
 
 function FoodList({ foods, deleteFood, addNewFood }) {
+  const [showForm, setShowForm] = useState(true);
+
+  const toggleForm = () => {
+    setShowForm((prevShow) => !prevShow);
+  };
   return (
     <>
-      <h1>LAB | React IronNutrition</h1>
-      <AddFoodForm addNewFood={addNewFood} />
-      {foods.map((item) => (
+      <Divider>LAB | React IronNutrition</Divider>
+      <Button onClick={toggleForm}>
+        {showForm ? "Hide Form" : "Add New Food"}
+      </Button>
+      <Divider></Divider>
+      {showForm && <AddFoodForm addNewFood={addNewFood} />}
+      {foods.map((item, index) => (
         <FoodBox key={item.id || index} food={item} deleteFood={deleteFood} />
       ))}
     </>
